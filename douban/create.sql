b1964bbb3c3641641639c32b9933dee7a36de8ec
AcerFeng <qinlanfeng@foxmail.com>
2017-09-08 19:31:17 +0800

    新增：评论、豆瓣用户表

--- douban/create.sql
+++ douban/create.sql
@@ -14,3 +14,22 @@ CREATE TABLE `douban_video` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `douban_id` (`video_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `douban_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `douban_user_id` varchar(20) DEFAULT NULL,
+  `douban_url` varchar(100) DEFAULT NULL,
+  `douban_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `douban_comments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(20) NOT NULL DEFAULT '',
+  `status` varchar(5) DEFAULT NULL,
+  `time` varchar(18) DEFAULT NULL,
+  `like_count` varchar(10) DEFAULT NULL,
+  `content` varchar(150) DEFAULT NULL,
+  `video_id` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
